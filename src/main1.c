74dbc7a2191c052b3454363cf0728646736222ea
leeziao <leeziao0331@gmail.com>
2023-03-27 13:59:26 +0800

    feat: first version

---
+++ src/main1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	printf("Hello World 1\n");
+	printf("Hello World %d\n", 2);
+	return 0;
+}
---
+++ src/main3.c
@@ -0,0 +1,8 @@
+// #include <stdio.h>
+int printf (const char *__restrict __format, ...);
+
+int main() {
+	printf("Hello World 1\n");
+	printf("Hello World %d\n", 2);
+	return 0;
+}
